7a019654e83270a2673a94c0873400cdc31cd11e
rishu-ish <dev.d.rishi.7@gmail.com>
2025-06-30 01:15:45 +0530

    Fix error

--- src/app/word/[term]/page.tsx
+++ src/app/word/[term]/page.tsx
@@ -1,6 +1,9 @@
-// app/word/[term]/page.tsx
-import WordDetailsDisplay from "@/components/WordDetailsDisplay";
+import WordDetailsDisplay from '@/components/WordDetailsDisplay';
+import { useParams } from 'next/navigation';
 
-export default function WordResultPage({ params }: { params: { term: string } }) {
-  return <WordDetailsDisplay term={params.term} />;
+export default function WordResultPage() {
+  const params = useParams();
+  const term = typeof params.term === 'string' ? params.term : '';
+
+  return <WordDetailsDisplay term={term} />;
 }
